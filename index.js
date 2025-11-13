95dea2f4a1d55782bdad33183c5cd86e0ccef0b6
J1mk0lakos <91275092+J1mk0lakos@users.noreply.github.com>
2025-11-13 14:43:20 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,459 @@
+const word1span1 = document.getElementById("word1span1");
+const word1span2 = document.getElementById("word1span2");
+const word1span3 = document.getElementById("word1span3");
+const word1span4 = document.getElementById("word1span4");
+const word1span5 = document.getElementById("word1span5");
+
+const word2span1 = document.getElementById("word2span1");
+const word2span2 = document.getElementById("word2span2");
+const word2span3 = document.getElementById("word2span3");
+const word2span4 = document.getElementById("word2span4");
+const word2span5 = document.getElementById("word2span5");
+
+const word3span1 = document.getElementById("word3span1");
+const word3span2 = document.getElementById("word3span2");
+const word3span3 = document.getElementById("word3span3");
+const word3span4 = document.getElementById("word3span4");
+const word3span5 = document.getElementById("word3span5");
+
+const word4span1 = document.getElementById("word4span1");
+const word4span2 = document.getElementById("word4span2");
+const word4span3 = document.getElementById("word4span3");
+const word4span4 = document.getElementById("word4span4");
+const word4span5 = document.getElementById("word4span5");
+
+const word5span1 = document.getElementById("word5span1");
+const word5span2 = document.getElementById("word5span2");
+const word5span3 = document.getElementById("word5span3");
+const word5span4 = document.getElementById("word5span4");
+const word5span5 = document.getElementById("word5span5");
+
+const startButton = document.getElementById("startBTN");
+const mainBoard = document.getElementById("mainBoard");
+const userInputDiv = document.getElementById("userInputDiv");
+const submitBTN = document.getElementById("submitBTN");
+const userInput = document.getElementById("userInput");
+const guideButton = document.getElementById("guideBTN");
+const guideDIV = document.getElementById("guideDIV1");
+
+const words = ['ΣΠΙΤΙ', 'ΒΙΒΛΙΟ', 'ΡΑΔΙΟ', 'ΦΙΛΟΣ'];
+
+startButton.onclick = function(){
+    guideButton.style.display = 'none'
+    startButton.style.display = 'none';
+    mainBoard.style.display = 'flex';
+    guideDIV.style.display = 'none';
+    userInputDiv.style.display = 'block';
+    startGame();
+
+}
+
+guideButton.onclick = function(){
+    guideButton.style.display = 'none'
+    mainBoard.style.display = 'none';
+    userInputDiv.style.display = 'none';
+    guideDIV.style.display = 'block';
+}
+
+var startGame = function(){
+    console.log("Game Started!")    
+    var randomWordNumber = Math.ceil(words.length * Math.random());
+    var randomWord = words[randomWordNumber];
+    var tries = 0;
+    var userWord = '';
+    console.log(randomWord)
+    submitBTN.onclick = function(){
+            userWord = userInput.value
+            userInput.value = "";
+            if (userWord.length == 5){
+                if (userWord === userWord.toUpperCase()){
+                    tries++;
+                    if (userWord==randomWord){
+                        alert("Congratulations! You have won!")
+                        startButton.style.display = 'inline-block';
+                        mainBoard.style.display = 'none';
+                        userInputDiv.style.display = 'none';
+                        cleartable();
+                    } else {
+                        if (tries == 1){
+                            word1(randomWord, userWord);
+                        } else if (tries == 2){
+                            word2(randomWord, userWord);
+                        } else if (tries == 3){
+                            word3(randomWord, userWord);
+                        } else if (tries == 4){
+                            word4(randomWord, userWord);
+                        } else if (tries == 5){
+                            word5(randomWord, userWord);
+                        } else {
+                            alert("You don't have any other tries left!")
+                            startButton.style.display = 'inline-block';
+                            mainBoard.style.display = 'none';
+                            userInputDiv.style.display = 'none';
+                            cleartable();
+                        }
+                    }
+                } else{
+                    userWord = userWord.toUpperCase();
+                    tries++;
+                    if (userWord==randomWord){
+                        alert("Congratulations! You have won!")
+                        startButton.style.display = 'inline-block';
+                        mainBoard.style.display = 'none';
+                        userInputDiv.style.display = 'none';
+                        cleartable();
+                    } else {
+                        if (tries == 1){
+                            word1(randomWord, userWord);
+                        } else if (tries == 2){
+                            word2(randomWord, userWord);
+                        } else if (tries == 3){
+                            word3(randomWord, userWord);
+                        } else if (tries == 4){
+                            word4(randomWord, userWord);
+                        } else if (tries == 5){
+                            word5(randomWord, userWord);
+                        } else {
+                            alert("You don't have any other tries left!")
+                            startButton.style.display = 'inline-block';
+                            mainBoard.style.display = 'none';
+                            userInputDiv.style.display = 'none';
+                            cleartable();
+                        }
+                    }
+                }
+            } else {
+                alert("The given word MUST have 5 letters.")
+                startButton.style.display = 'inline-block';
+                mainBoard.style.display = 'none';
+                userInputDiv.style.display = 'none';
+                cleartable();
+            }
+
+    }
+}
+
+var word1 = function(randomWord, userWord){
+    var rightWord1 = randomWord.charAt(0)
+    var rightWord2 = randomWord.charAt(1)
+    var rightWord3 = randomWord.charAt(2)
+    var rightWord4 = randomWord.charAt(3)
+    var rightWord5 = randomWord.charAt(4)
+    if (userWord.charAt(0) == rightWord1){
+        word1span1.style.backgroundColor  = "green"
+        word1span1.textContent = userWord.charAt(0)
+    } else if(randomWord.includes(userWord.charAt(0))){
+        word1span1.style.backgroundColor = "orange"
+        word1span1.textContent = userWord.charAt(0)
+    } else{
+        word1span1.textContent = userWord.charAt(0)
+    }
+    if (userWord.charAt(1) == rightWord2){
+        word1span2.style.backgroundColor = "green"
+        word1span2.textContent = userWord.charAt(1)
+    } else if(randomWord.includes(userWord.charAt(1))){
+        word1span2.style.backgroundColor = "orange"
+        word1span2.textContent = userWord.charAt(1)
+    } else{
+        word1span2.textContent = userWord.charAt(1)
+    }
+    if (userWord.charAt(2) == rightWord3){
+        word1span3.style.backgroundColor = "green"
+        word1span3.textContent = userWord.charAt(2)
+    } else if(randomWord.includes(userWord.charAt(2))){
+        word1span3.style.backgroundColor = "orange"
+        word1span3.textContent = userWord.charAt(2)
+    } else{
+        word1span3.textContent = userWord.charAt(2)
+    }
+    if (userWord.charAt(3) == rightWord4){
+        word1span4.style.backgroundColor = "green"
+        word1span4.textContent = userWord.charAt(3)
+    } else if(randomWord.includes(userWord.charAt(3))){
+        word1span4.style.backgroundColor = "orange"
+        word1span4.textContent = userWord.charAt(3)
+    } else{
+        word1span4.textContent = userWord.charAt(3)
+    }
+    if (userWord.charAt(4) == rightWord5){
+        word1span5.style.backgroundColor = "green"
+        word1span5.textContent = userWord.charAt(4)
+    } else if(randomWord.includes(userWord.charAt(4))){
+        word1span5.style.backgroundColor = "orange"
+        word1span5.textContent = userWord.charAt(4)
+    } else{
+        word1span5.textContent = userWord.charAt(4)
+    }    
+}
+
+var word2 = function(randomWord, userWord){
+    var rightWord1 = randomWord.charAt(0)
+    var rightWord2 = randomWord.charAt(1)
+    var rightWord3 = randomWord.charAt(2)
+    var rightWord4 = randomWord.charAt(3)
+    var rightWord5 = randomWord.charAt(4)
+    if (userWord.charAt(0) == rightWord1){
+        word2span1.style.backgroundColor = "green"
+        word2span1.textContent = userWord.charAt(0)
+    } else if(randomWord.includes(userWord.charAt(0))){
+        word2span1.style.backgroundColor = "orange"
+        word2span1.textContent = userWord.charAt(0)
+    } else{
+        word2span1.textContent = userWord.charAt(0)
+    }
+    if (userWord.charAt(1) == rightWord2){
+        word2span2.style.backgroundColor = "green"
+        word2span2.textContent = userWord.charAt(1)
+    } else if(randomWord.includes(userWord.charAt(1))){
+        word2span2.style.backgroundColor = "orange"
+        word2span2.textContent = userWord.charAt(1)
+    } else{
+        word2span2.textContent = userWord.charAt(1)
+    }
+    if (userWord.charAt(2) == rightWord3){
+        word2span3.style.backgroundColor = "green"
+        word2span3.textContent = userWord.charAt(2)
+    } else if(randomWord.includes(userWord.charAt(2))){
+        word2span3.style.backgroundColor = "orange"
+        word2span3.textContent = userWord.charAt(2)
+    } else{
+        word2span3.textContent = userWord.charAt(2)
+    }
+    if (userWord.charAt(3) == rightWord4){
+        word2span4.style.backgroundColor = "green"
+        word2span4.textContent = userWord.charAt(3)
+    } else if(randomWord.includes(userWord.charAt(3))){
+        word2span4.style.backgroundColor = "orange"
+        word2span4.textContent = userWord.charAt(3)
+    } else{
+        word2span4.textContent = userWord.charAt(3)
+    }
+    if (userWord.charAt(4) == rightWord5){
+        word2span5.style.backgroundColor = "green"
+        word2span5.textContent = userWord.charAt(4)
+    } else if(randomWord.includes(userWord.charAt(4))){
+        word2span5.style.backgroundColor = "orange"
+        word2span5.textContent = userWord.charAt(4)
+    } else{
+        word2span5.textContent = userWord.charAt(4)
+    }    
+}
+
+var word3 = function(randomWord, userWord){
+    var rightWord1 = randomWord.charAt(0)
+    var rightWord2 = randomWord.charAt(1)
+    var rightWord3 = randomWord.charAt(2)
+    var rightWord4 = randomWord.charAt(3)
+    var rightWord5 = randomWord.charAt(4)
+    if (userWord.charAt(0) == rightWord1){
+        word3span1.style.backgroundColor = "green"
+        word3span1.textContent = userWord.charAt(0)
+    } else if(randomWord.includes(userWord.charAt(0))){
+        word3span1.style.backgroundColor = "orange"
+        word3span1.textContent = userWord.charAt(0)
+    } else{
+        word3span1.textContent = userWord.charAt(0)
+    }
+    if (userWord.charAt(1) == rightWord2){
+        word3span2.style.backgroundColor = "green"
+        word3span2.textContent = userWord.charAt(1)
+    } else if(randomWord.includes(userWord.charAt(1))){
+        word3span2.style.backgroundColor = "orange"
+        word3span2.textContent = userWord.charAt(1)
+    } else{
+        word3span2.textContent = userWord.charAt(1)
+    }
+    if (userWord.charAt(2) == rightWord3){
+        word3span3.style.backgroundColor = "green"
+        word3span3.textContent = userWord.charAt(2)
+    } else if(randomWord.includes(userWord.charAt(2))){
+        word3span3.style.backgroundColor = "orange"
+        word3span3.textContent = userWord.charAt(2)
+    } else{
+        word3span3.textContent = userWord.charAt(2)
+    }
+    if (userWord.charAt(3) == rightWord4){
+        word3span4.style.backgroundColor = "green"
+        word3span4.textContent = userWord.charAt(3)
+    } else if(randomWord.includes(userWord.charAt(3))){
+        word3span4.style.backgroundColor = "orange"
+        word3span4.textContent = userWord.charAt(3)
+    } else{
+        word3span4.textContent = userWord.charAt(3)
+    }
+    if (userWord.charAt(4) == rightWord5){
+        word3span5.style.backgroundColor = "green"
+        word3span5.textContent = userWord.charAt(4)
+    } else if(randomWord.includes(userWord.charAt(4))){
+        word3span5.style.backgroundColor = "orange"
+        word3span5.textContent = userWord.charAt(4)
+    } else{
+        word3span5.textContent = userWord.charAt(4)
+    }    
+}
+
+var word4 = function(randomWord, userWord){
+    var rightWord1 = randomWord.charAt(0)
+    var rightWord2 = randomWord.charAt(1)
+    var rightWord3 = randomWord.charAt(2)
+    var rightWord4 = randomWord.charAt(3)
+    var rightWord5 = randomWord.charAt(4)
+    if (userWord.charAt(0) == rightWord1){
+        word4span1.style.backgroundColor = "green"
+        word4span1.textContent = userWord.charAt(0)
+    } else if(randomWord.includes(userWord.charAt(0))){
+        word4span1.style.backgroundColor = "orange"
+        word4span1.textContent = userWord.charAt(0)
+    } else{
+        word4span1.textContent = userWord.charAt(0)
+    }
+    if (userWord.charAt(1) == rightWord2){
+        word4span2.style.backgroundColor = "green"
+        word4span2.textContent = userWord.charAt(1)
+    } else if(randomWord.includes(userWord.charAt(1))){
+        word4span2.style.backgroundColor = "orange"
+        word4span2.textContent = userWord.charAt(1)
+    } else{
+        word4span2.textContent = userWord.charAt(1)
+    }
+    if (userWord.charAt(2) == rightWord3){
+        word4span3.style.backgroundColor = "green"
+        word4span3.textContent = userWord.charAt(2)
+    } else if(randomWord.includes(userWord.charAt(2))){
+        word4span3.style.backgroundColor = "orange"
+        word4span3.textContent = userWord.charAt(2)
+    } else{
+        word4span3.textContent = userWord.charAt(2)
+    }
+    if (userWord.charAt(3) == rightWord4){
+        word4span4.style.backgroundColor = "green"
+        word4span4.textContent = userWord.charAt(3)
+    } else if(randomWord.includes(userWord.charAt(3))){
+        word4span4.style.backgroundColor = "orange"
+        word4span4.textContent = userWord.charAt(3)
+    } else{
+        word4span4.textContent = userWord.charAt(3)
+    }
+    if (userWord.charAt(4) == rightWord5){
+        word4span5.style.backgroundColor = "green"
+        word4span5.textContent = userWord.charAt(4)
+    } else if(randomWord.includes(userWord.charAt(4))){
+        word4span5.style.backgroundColor = "orange"
+        word4span5.textContent = userWord.charAt(4)
+    } else{
+        word4span5.textContent = userWord.charAt(4)
+    }    
+}
+
+var word5 = function(randomWord, userWord){
+    var rightWord1 = randomWord.charAt(0)
+    var rightWord2 = randomWord.charAt(1)
+    var rightWord3 = randomWord.charAt(2)
+    var rightWord4 = randomWord.charAt(3)
+    var rightWord5 = randomWord.charAt(4)
+    if (userWord.charAt(0) == rightWord1){
+        word5span1.style.backgroundColor = "green"
+        word5span1.textContent = userWord.charAt(0)
+    } else if(randomWord.includes(userWord.charAt(0))){
+        word5span1.style.backgroundColor = "orange"
+        word5span1.textContent = userWord.charAt(0)
+    } else{
+        word5span1.textContent = userWord.charAt(0)
+    }
+    if (userWord.charAt(1) == rightWord2){
+        word5span2.style.backgroundColor = "green"
+        word5span2.textContent = userWord.charAt(1)
+    } else if(randomWord.includes(userWord.charAt(1))){
+        word5span2.style.backgroundColor = "orange"
+        word5span2.textContent = userWord.charAt(1)
+    } else{
+        word5span2.textContent = userWord.charAt(1)
+    }
+    if (userWord.charAt(2) == rightWord3){
+        word5span3.style.backgroundColor = "green"
+        word5span3.textContent = userWord.charAt(2)
+    } else if(randomWord.includes(userWord.charAt(2))){
+        word5span3.style.backgroundColor = "orange"
+        word5span3.textContent = userWord.charAt(2)
+    } else{
+        word5span3.textContent = userWord.charAt(2)
+    }
+    if (userWord.charAt(3) == rightWord4){
+        word5span4.style.backgroundColor = "green"
+        word5span4.textContent = userWord.charAt(3)
+    } else if(randomWord.includes(userWord.charAt(3))){
+        word5span4.style.backgroundColor = "orange"
+        word5span4.textContent = userWord.charAt(3)
+    } else{
+        word5span4.textContent = userWord.charAt(3)
+    }
+    if (userWord.charAt(4) == rightWord5){
+        word5span5.style.backgroundColor = "green"
+        word5span5.textContent = userWord.charAt(4)
+    } else if(randomWord.includes(userWord.charAt(4))){
+        word5span5.style.backgroundColor = "orange"
+        word5span5.textContent = userWord.charAt(4)
+    } else{
+        word5span5.textContent = userWord.charAt(4)
+    }    
+}
+
+var cleartable = function(){
+    word1span1.textContent = '';
+    word1span2.textContent = '';
+    word1span3.textContent = '';
+    word1span4.textContent = '';
+    word1span5.textContent = '';
+    word1span1.style.backgroundColor = "lightgray"
+    word1span2.style.backgroundColor = "lightgray"
+    word1span3.style.backgroundColor = "lightgray"
+    word1span4.style.backgroundColor = "lightgray"
+    word1span5.style.backgroundColor = "lightgray"
+
+    word2span1.textContent = '';
+    word2span2.textContent = '';
+    word2span3.textContent = '';
+    word2span4.textContent = '';
+    word2span5.textContent = '';
+    word2span1.style.backgroundColor = "lightgray"
+    word2span2.style.backgroundColor = "lightgray"
+    word2span3.style.backgroundColor = "lightgray"
+    word2span4.style.backgroundColor = "lightgray"
+    word2span5.style.backgroundColor = "lightgray"
+
+    word3span1.textContent = '';
+    word3span2.textContent = '';
+    word3span3.textContent = '';
+    word3span4.textContent = '';
+    word3span5.textContent = '';
+    word3span1.style.backgroundColor = "lightgray"
+    word3span2.style.backgroundColor = "lightgray"
+    word3span3.style.backgroundColor = "lightgray"
+    word3span4.style.backgroundColor = "lightgray"
+    word3span5.style.backgroundColor = "lightgray"
+
+    word4span1.textContent = '';
+    word4span2.textContent = '';
+    word4span3.textContent = '';
+    word4span4.textContent = '';
+    word4span5.textContent = '';
+    word4span1.style.backgroundColor = "lightgray"
+    word4span2.style.backgroundColor = "lightgray"
+    word4span3.style.backgroundColor = "lightgray"
+    word4span4.style.backgroundColor = "lightgray"
+    word4span5.style.backgroundColor = "lightgray"
+
+    word5span1.textContent = '';
+    word5span2.textContent = '';
+    word5span3.textContent = '';
+    word5span4.textContent = '';
+    word5span5.textContent = '';
+    word5span1.style.backgroundColor = "lightgray"
+    word5span2.style.backgroundColor = "lightgray"
+    word5span3.style.backgroundColor = "lightgray"
+    word5span4.style.backgroundColor = "lightgray"
+    word5span5.style.backgroundColor = "lightgray"
+}
+
+var word1char = words.charAt(1)
+console.log(word1char)
